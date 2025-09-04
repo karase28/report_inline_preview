9d43f0c2fd9e08ba705046f6ef85279260019d82
karase28 <jkopacz@os.pl>
2025-09-04 19:54:57 +0200

    dodałem obsługę przycisku

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,1 +1,3 @@
 from . import report
+from . import controllers
+from . import models
---
+++ models/stock_picking.py
@@ -0,0 +1,20 @@
+from odoo import models
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def action_print_inline_wz(self):
+        """Podgląd WZ w przeglądarce"""
+        return {
+            "type": "ir.actions.act_url",
+            "url": "/report/inline/stock.report_deliveryslip/%s" % self.id,
+            "target": "new",  # otworzy w nowej zakładce
+        }
+
+    def action_print_download_wz(self):
+        """Pobierz WZ jako PDF"""
+        return {
+            "type": "ir.actions.act_url",
+            "url": "/report/download/stock.report_deliveryslip/%s" % self.id,
+            "target": "self",  # wymusi pobranie
+        }
